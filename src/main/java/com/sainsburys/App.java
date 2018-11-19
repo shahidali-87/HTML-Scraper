737797acd126e5c8aeb9766db85c1b8f4f801d2d
shahidali-87 <shahid87+github@gmail.com>
2018-11-19 13:00:52 +0000

    Initial commit - create Maven project

---
+++ src/main/java/com/sainsburys/App.java
@@ -0,0 +1,13 @@
+package com.sainsburys;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
